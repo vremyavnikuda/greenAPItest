37f56236839134b001a66579b9cfbbd12fe7249b
vremyavnikuda <andrew.nevsky@yandex.ru>
2024-07-02 21:41:29 +0700

    upd final

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,17 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/template/html/v2"
 	"github.com/pterm/pterm"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 )
 
 const (
-	apiUrl   = "https://1103.api.green-api.com"
-	apiMedia = "https://1103.media.green-api.com"
+	apiUrl           = "https://1103.api.green-api.com"
+	sendFileByUrlAPI = "https://1103.api.green-api.com"
 )
 
 type SettingsResponse struct {
@@ -97,6 +97,12 @@ func main() {
 		Views: engine,
 	})
 
+	// Add CORS middleware
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+	}))
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("template", nil)
 	})
@@ -106,23 +112,23 @@ func main() {
 	app.Get("/showMessagesQueue", showMessagesQueue)
 	app.Get("/clearMessagesQueue", clearMessagesQueue)
 	app.Post("/sendMessage", sendMessage)
-	app.Post("/sendFileByUrl", sendFileByUrl)
+	app.Post("/proxy/sendFileByUrl", sendFileByUrlProxy)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	log.Printf("Server started on port %s\n", port)
-	log.Fatal(app.Listen(":" + port))
+	pterm.Info.Printf("Сервер запущен на порту %s\n", port)
+	pterm.Fatal.Println(app.Listen(":" + port))
 }
 
 func getSettings(c *fiber.Ctx) error {
 	idInstance := c.Get("X-IdInstance")
 	apiTokenInstance := c.Get("X-ApiTokenInstance")
 	if idInstance == "" || apiTokenInstance == "" {
-		log.Println("Несанкционированный запрос: отсутствует idInstance или apiTokenInstance.")
-		return c.Status(fiber.StatusUnauthorized).SendString("Отсутствует заголовок IdInstance или ApiTokenInstance.")
+		pterm.Error.Println("Несанкционированный запрос: отсутствует idInstance или apiTokenInstance.")
+		return c.Status(fiber.StatusUnauthorized).SendString("Отсутствует заголовок IdInstance или ApiTokenInstance..")
 	}
 
 	fullUrl := fmt.Sprintf("%s/waInstance%s/getSettings/%s", apiUrl, idInstance, apiTokenInstance)
@@ -133,8 +139,8 @@ func getStateInstance(c *fiber.Ctx) error {
 	idInstance := c.Get("X-IdInstance")
 	apiTokenInstance := c.Get("X-ApiTokenInstance")
 	if idInstance == "" || apiTokenInstance == "" {
-		log.Println("Несанкционированный запрос: отсутствует idInstance или apiTokenInstance.")
-		return c.Status(fiber.StatusUnauthorized).SendString("Отсутствует заголовок IdInstance или ApiTokenInstance.")
+		pterm.Error.Println("Несанкционированный запрос: отсутствует idInstance или apiTokenInstance.")
+		return c.Status(fiber.StatusUnauthorized).SendString("Отсутствует заголовок IdInstance или ApiTokenInstance..")
 	}
 
 	fullUrl := fmt.Sprintf("%s/waInstance%s/getStateInstance/%s", apiUrl, idInstance, apiTokenInstance)
@@ -145,7 +151,7 @@ func showMessagesQueue(c *fiber.Ctx) error {
 	idInstance := c.Get("X-IdInstance")
 	apiTokenInstance := c.Get("X-ApiTokenInstance")
 	if idInstance == "" || apiTokenInstance == "" {
-		log.Println("Несанкционированный запрос: отсутствует idInstance или apiTokenInstance.")
+		pterm.Error.Println("Несанкционированный запрос: отсутствует idInstance или apiTokenInstance.")
 		return c.Status(fiber.StatusUnauthorized).SendString("Отсутствует заголовок IdInstance или ApiTokenInstance.")
 	}
 
@@ -157,8 +163,8 @@ func clearMessagesQueue(c *fiber.Ctx) error {
 	idInstance := c.Get("X-IdInstance")
 	apiTokenInstance := c.Get("X-ApiTokenInstance")
 	if idInstance == "" || apiTokenInstance == "" {
-		log.Println("Несанкционированный запрос: отсутствует idInstance или apiTokenInstance.")
-		return c.Status(fiber.StatusUnauthorized).SendString("Отсутствует заголовок IdInstance или ApiTokenInstance.")
+		pterm.Error.Println("Несанкционированный запрос: отсутствует idInstance или apiTokenInstance.")
+		return c.Status(fiber.StatusUnauthorized).SendString("Отсутствует заголовок IdInstance или ApiTokenInstance..")
 	}
 
 	fullUrl := fmt.Sprintf("%s/waInstance%s/clearMessagesQueue/%s", apiUrl, idInstance, apiTokenInstance)
@@ -169,24 +175,22 @@ func sendMessage(c *fiber.Ctx) error {
 	idInstance := c.Get("X-IdInstance")
 	apiTokenInstance := c.Get("X-ApiTokenInstance")
 	if idInstance == "" || apiTokenInstance == "" {
-		log.Println("Unauthorized request: Missing idInstance or apiTokenInstance")
-		return c.Status(fiber.StatusUnauthorized).SendString("IdInstance or ApiTokenInstance header missing")
+		pterm.Error.Println("Несанкционированный запрос: отсутствует idInstance или apiTokenInstance.")
+		return c.Status(fiber.StatusUnauthorized).SendString("Отсутствует заголовок IdInstance или ApiTokenInstance.")
 	}
 
 	var body struct {
-		ChatID          string `json:"chatId"`
-		Message         string `json:"message"`
-		QuotedMessageID string `json:"quotedMessageId,omitempty"`
-		LinkPreview     bool   `json:"linkPreview,omitempty"`
+		ChatID  string `json:"chatId"`
+		Message string `json:"message"`
 	}
 
 	if err := c.BodyParser(&body); err != nil {
-		log.Printf("Bad request: %v\n", err)
+		pterm.Error.Printf("Неверный запрос: %v\n", err)
 		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
 	}
 
 	if body.ChatID == "" || body.Message == "" {
-		return c.Status(fiber.StatusBadRequest).SendString("chatId and message are required fields")
+		return c.Status(fiber.StatusBadRequest).SendString("ChatId и сообщение являются обязательными полями.")
 	}
 
 	fullUrl := fmt.Sprintf("%s/waInstance%s/sendMessage/%s", apiUrl, idInstance, apiTokenInstance)
@@ -195,34 +199,33 @@ func sendMessage(c *fiber.Ctx) error {
 	return makeAPIRequest(c, fullUrl, body, "POST", &SendMessageResponse{})
 }
 
-func sendFileByUrl(c *fiber.Ctx) error {
+func sendFileByUrlProxy(c *fiber.Ctx) error {
 	idInstance := c.Get("X-IdInstance")
 	apiTokenInstance := c.Get("X-ApiTokenInstance")
 	if idInstance == "" || apiTokenInstance == "" {
-		log.Println("Unauthorized request: Missing idInstance or apiTokenInstance")
-		return c.Status(fiber.StatusUnauthorized).SendString("IdInstance or ApiTokenInstance header missing")
+		pterm.Error.Println("Несанкционированный запрос: отсутствует idInstance или apiTokenInstance.")
+		return c.Status(fiber.StatusUnauthorized).SendString("Отсутствует заголовок IdInstance или ApiTokenInstance.")
 	}
 
 	var body struct {
 		ChatID   string `json:"chatId"`
 		URLFile  string `json:"urlFile"`
-		FileName string `json:"fileName,omitempty"`
-		Caption  string `json:"caption,omitempty"`
+		FileName string `json:"fileName"`
 	}
 
 	if err := c.BodyParser(&body); err != nil {
-		log.Printf("Bad request: %v\n", err)
+		pterm.Error.Printf("Неверный запрос: %v\n", err)
 		return c.Status(fiber.StatusBadRequest).SendString(err.Error())
 	}
 
-	if body.ChatID == "" || body.URLFile == "" {
-		return c.Status(fiber.StatusBadRequest).SendString("chatId and urlFile are required fields")
+	if body.ChatID == "" || body.URLFile == "" || body.FileName == "" {
+		return c.Status(fiber.StatusBadRequest).SendString("ChatId, urlFile и fileName являются обязательными полями.")
 	}
 
-	fullUrl := fmt.Sprintf("%s/waInstance%s/sendFileByUrl/%s", apiMedia, idInstance, apiTokenInstance)
+	fullUrl := fmt.Sprintf("%s/waInstance%s/sendFileByUrl/%s", sendFileByUrlAPI, idInstance, apiTokenInstance)
 	pterm.Info.Println(fullUrl)
 	pterm.Info.Println(body)
-	return makeAPIRequest(c, fullUrl, body, "POST", &SendMessageResponse{})
+	return makeAPIRequestWithHeaders(c, fullUrl, body, "POST", &SendMessageResponse{}, idInstance, apiTokenInstance)
 }
 
 func makeAPIRequest(c *fiber.Ctx, url string, body interface{}, method string, responseStruct interface{}) error {
@@ -232,14 +235,14 @@ func makeAPIRequest(c *fiber.Ctx, url string, body interface{}, method string, r
 	if body != nil {
 		reqBody, err = json.Marshal(body)
 		if err != nil {
-			log.Printf("Internal server error: %v\n", err)
+			pterm.Error.Printf("Внутренняя ошибка сервера: %v\n", err)
 			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 		}
 	}
 
 	req, err := http.NewRequest(method, url, bytes.NewBuffer(reqBody))
 	if err != nil {
-		log.Printf("Internal server error: %v\n", err)
+		pterm.Error.Printf("Внутренняя ошибка сервера: %v\n", err)
 		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 	}
 
@@ -248,52 +251,81 @@ func makeAPIRequest(c *fiber.Ctx, url string, body interface{}, method string, r
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Printf("Request to API failed: %v\n", err)
+		pterm.Error.Printf("Запрос к API не удался: %v\n", err)
 		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 	}
 	defer resp.Body.Close()
 
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Printf("Reading API response failed: %v\n", err)
+		pterm.Error.Printf("Не удалось прочитать ответ API.: %v\n", err)
 		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 	}
 
-	log.Printf("API response status: %d", resp.StatusCode)
-	log.Printf("API response body: %s", respBody)
+	pterm.Info.Printf("Статус ответа API: %d\n", resp.StatusCode)
+	pterm.Info.Printf("Тело ответа API: %s\n", respBody)
 
 	if resp.StatusCode != http.StatusOK {
-		// Handle specific 466 status code error
-		if resp.StatusCode == 466 {
-			var quotaError struct {
-				InvokeStatus struct {
-					Method string `json:"method"`
-					Used   int    `json:"used"`
-					Total  int    `json:"total"`
-					Status string `json:"status"`
-				} `json:"invokeStatus"`
-				CorrespondentsStatus struct {
-					Method      string `json:"method"`
-					Used        int    `json:"used"`
-					Total       int    `json:"total"`
-					Status      string `json:"status"`
-					Description string `json:"description"`
-				} `json:"correspondentsStatus"`
-			}
-			if err := json.Unmarshal(respBody, &quotaError); err != nil {
-				log.Printf("Decoding quota error response failed: %v\n", err)
-				return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
-			}
-			return c.Status(resp.StatusCode).JSON(quotaError)
-		}
-
-		log.Printf("Received non-200 response: %d\n", resp.StatusCode)
-		return c.Status(resp.StatusCode).SendString(fmt.Sprintf("Error response from API: %s", respBody))
+		pterm.Error.Printf("Получен ответ, отличный от 200: %d\n", resp.StatusCode)
+		return c.Status(resp.StatusCode).SendString(pterm.Error.Sprintf("Ответ об ошибке от API: %s\n", respBody))
 	}
 
 	err = json.Unmarshal(respBody, responseStruct)
 	if err != nil {
-		log.Printf("Decoding API response failed: %v\n", err)
+		pterm.Error.Printf("Не удалось декодировать ответ API.: %v\n", err)
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	return c.JSON(responseStruct)
+}
+
+func makeAPIRequestWithHeaders(c *fiber.Ctx, url string, body interface{}, method string, responseStruct interface{}, idInstance, apiTokenInstance string) error {
+	var reqBody []byte
+	var err error
+
+	if body != nil {
+		reqBody, err = json.Marshal(body)
+		if err != nil {
+			pterm.Error.Printf("Внутренняя ошибка сервера: %v\n", err)
+			return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+		}
+	}
+
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(reqBody))
+	if err != nil {
+		pterm.Error.Printf("Внутренняя ошибка сервера: %v\n", err)
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-IdInstance", idInstance)
+	req.Header.Set("X-ApiTokenInstance", apiTokenInstance)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		pterm.Error.Printf("Запрос к API не удался: %v\n", err)
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		pterm.Error.Printf("Не удалось прочитать ответ API.: %v\n", err)
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	pterm.Info.Printf("Статус ответа API: %d\n", resp.StatusCode)
+	pterm.Info.Printf("Тело ответа API: %s\n", respBody)
+
+	if resp.StatusCode != http.StatusOK {
+		pterm.Error.Printf("Получен ответ, отличный от 200: %d \n", resp.StatusCode)
+		return c.Status(resp.StatusCode).SendString(fmt.Sprintf("Ответ об ошибке от API: %s\n", respBody))
+	}
+
+	err = json.Unmarshal(respBody, responseStruct)
+	if err != nil {
+		pterm.Error.Printf("Не удалось декодировать ответ API.: %v \n", err)
 		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 	}
 
--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.22
 
 require github.com/gofiber/fiber/v2 v2.52.5
 
-require github.com/gofiber/template/html/v2 v2.1.1
+require (
+	github.com/gofiber/template/html/v2 v2.1.1
+	github.com/pterm/pterm v0.12.79
+)
 
 require (
 	atomicgo.dev/cursor v0.2.0 // indirect
@@ -14,7 +17,6 @@ require (
 	github.com/containerd/console v1.0.3 // indirect
 	github.com/gofiber/template v1.8.3 // indirect
 	github.com/gofiber/utils v1.1.0 // indirect
-	github.com/gofiber/utils/v2 v2.0.0-beta.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gookit/color v1.5.4 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
@@ -22,8 +24,8 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/pterm/pterm v0.12.79 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.55.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
@@ -31,5 +33,4 @@ require (
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/term v0.16.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
